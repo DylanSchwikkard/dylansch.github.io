d2f7bd676a26e184e33465ea59d3368d4e56c236
DylanSchwikkard <dwschwikkard@gmail.com>
2020-03-28 00:07:36 +0200

    Scroll-to-ID

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$( "#skill-link" ).click(function( event ) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: $('#Skills').offset().top }, 500);
+    });
+    
+    $( "#profile-link" ).click(function( event ) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: $('#Profile').offset().top }, 500);
+	});
+});
