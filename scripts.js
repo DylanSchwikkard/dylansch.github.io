d9307ceeaf3a233db6005df8e3149ab0d2e87932
DylanSchwikkard <dwschwikkard@gmail.com>
2020-03-28 13:33:24 +0200

    Messy Changes
    Needs tidying up

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$( "#skill-link" ).click(function( event ) {
 		event.preventDefault();
-		$("html, body").animate({ scrollTop: $('#Skills').offset().top }, 500);
+		$("html, body").animate({ scrollTop: $('#Skills').offset().top },500);
     });
     
     $( "#profile-link" ).click(function( event ) {
