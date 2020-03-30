122a4ddcb9a98e27e752bf7b40ec795256d96611
DylanSchwikkard <dwschwikkard@gmail.com>
2020-03-30 11:00:33 +0200

    positioning-fixed

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 	$( "#skill-link" ).click(function( event ) {
 		event.preventDefault();
-		$("html, body").animate({ scrollTop: $('#Skills').offset().top },500);
+		$("html, body").animate({ scrollTop: ($('#Skills').offset().top-59) },500);
     });
     
     $( "#profile-link" ).click(function( event ) {
 		event.preventDefault();
 		$("html, body").animate({ scrollTop: $('#Profile').offset().top }, 500);
 	});
+	$( "#edu-link" ).click(function( event ) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: ($('#Education').offset().top-59) }, 500);
+	});
+	$('.navbar-nav>a').on('click', function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+
 });
